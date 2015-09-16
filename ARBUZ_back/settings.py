903e58f70f7601f592370a2a02aff1e453f8adef
BGrigorovich <bohdan.hryhorovych@gmail.com>
2015-09-16 18:36:12 +0300

    Update settings.py

--- ARBUZ_back/settings.py
+++ ARBUZ_back/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     'PAGE_SIZE': 10
